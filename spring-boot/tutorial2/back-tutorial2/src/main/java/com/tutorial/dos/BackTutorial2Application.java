54f50314c338aef23d5780be7a33525220679d5f
dgarcia <manuonda@gmail.com>
2022-02-14 23:15:10 -0300

    init project tutorial2

---
+++ spring-boot/tutorial2/back-tutorial2/src/main/java/com/tutorial/dos/BackTutorial2Application.java
@@ -0,0 +1,13 @@
+package com.tutorial.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTutorial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTutorial2Application.class, args);
+	}
+
+}
---
+++ spring-boot/tutorial2/back-tutorial2/src/test/java/com/tutorial/dos/BackTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.dos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTutorial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
